9b53ce748e3393bcfa2446e634e56f5d9bc7fd8a
Jes Ramsing <jesramsing@hotmail.com>
2024-12-30 23:34:28 +0100

    use strict wrong ?

--- node-red-dfair.js
+++ node-red-dfair.js
@@ -1,6 +1,4 @@
 module.exports = function (RED) {
-  "use strict";
-
   function DFAirNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
@@ -8,35 +6,35 @@ module.exports = function (RED) {
     // node-specific code goes here
     this.status({ fill: "red", shape: "ring", text: "disconnected" });
     //start the HW driver (or perhaps wait until configuration requirements has been met)
-  
 
-  //   this.on("input", function (msg, send, done) {
-  //     // do something with 'msg'
+    //   this.on("input", function (msg, send, done) {
+    //     // do something with 'msg'
 
-  //     // If an error is hit, report it to the runtime
-  //     if (err) {
-  //       if (done) {
-  //         // Node-RED 1.0 compatible
-  //         done(err);
-  //       } else {
-  //         // Node-RED 0.x compatible
-  //         node.error(err, msg);
-  //       }
-  //     }
-  //   });
+    //     // If an error is hit, report it to the runtime
+    //     if (err) {
+    //       if (done) {
+    //         // Node-RED 1.0 compatible
+    //         done(err);
+    //       } else {
+    //         // Node-RED 0.x compatible
+    //         node.error(err, msg);
+    //       }
+    //     }
+    //   });
 
-  this.on("input", function (msg) {
-    this.send(msg);
-  });
+    this.on("input", function (msg) {
+      this.send(msg);
+    });
 
-  this.on("close", function (removed, done) {
-    if (removed) {
-      // This node has been disabled/deleted
-    } else {
-      // This node is being restarted
-    }
-    done();
-  });
-}
+    this.on("close", function (removed, done) {
+      if (removed) {
+        // This node has been disabled/deleted
+      } else {
+        // This node is being restarted
+      }
+      done();
+    });
+  }
+
   RED.nodes.registerType("dfair", DFAirNode);
 };
